d4fe8ec0708c8ef0c29c79ad63b1a8d16a77d56d
Chon <98424010+HenryVJ@users.noreply.github.com>
2023-03-30 20:15:04 +0700

    Update swap.js

--- rightclicker-master/netlify/functions/swap.js
+++ rightclicker-master/netlify/functions/swap.js
@@ -1,35 +1,21 @@
 const { schedule } = require("@netlify/functions");
-const { ethers } = require("ethers")
-require("dotenv").config()
+const { ethers } = require("ethers");
+require("dotenv").config();
 
-const IUniswapV2Router02 = require('@uniswap/v2-periphery/build/IUniswapV2Router02.json')
+const IERC20 = require('@openzeppelin/contracts/build/contracts/ERC20.json');
 
-const handler = async function (event, context) {
-  console.log("Received event:", event);
+const handler = async function () {
+  console.log("Running function...");
+  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
+  const ACCOUNT_1 = new ethers.Wallet(process.env.PRIVATE_KEY_1, provider);
+  const ACCOUNT_2 = new ethers.Wallet(process.env.PRIVATE_KEY_2, provider);
+  const ACCOUNT_3 = new ethers.Wallet(process.env.PRIVATE_KEY_3, provider);
+  const ERC20 = new ethers.Contract(process.env.TOKEN_ADDRESS, IERC20.abi);
+  await ERC20.connect(ACCOUNT_1).transfer(ACCOUNT_2.address, process.env.TRANSFER_AMOUNT);
+  await ERC20.connect(ACCOUNT_1).transfer(ACCOUNT_3.address, process.env.TRANSFER_AMOUNT);
+  console.log("Transfers Complete!");
+  console.log(`See transactions at: https://goerli.etherscan.io/address/${ACCOUNT_1.address}`);
+};
 
-  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
-
-  const ACCOUNT_1 = new ethers.Wallet(process.env.PRIVATE_KEY_1, provider)
-
-  const V2_ROUTER = new ethers.Contract(process.env.V2_ROUTER_ADDRESS, IUniswapV2Router02.abi)
-
-  const PATH = [process.env.WETH_ADDRESS, process.env.TOKEN_ADDRESS]
-  const DEADLINE = Math.floor(Date.now() / 1000) + 60 * 10 // 10 minutes
-
-  const transaction = await V2_ROUTER.connect(ACCOUNT_1).swapExactETHForTokens(
-    0,
-    PATH,
-    ACCOUNT_1.address,
-    DEADLINE,
-    { value: process.env.SWAP_AMOUNT }
-  )
-
-  console.log(`Swap Complete!`)
-  console.log(`See transaction at: https://goerli.etherscan.io/tx/${transaction.hash}/`)
-
-  return {
-    statusCode: 200,
-  }
-}
-
-exports.handler = schedule("@daily", handler)
+// Schedule the function to run at 7:00 AM UTC on Monday, Thursday, and Sunday
+exports.handler = schedule("cron(0 7 * * 1,4,7)", handler);
