67ee78745f443863c9a48be478918162fe76edc8
Chon <98424010+HenryVJ@users.noreply.github.com>
2023-03-31 00:53:50 +0700

    Update swap.js

--- rightclicker-master/netlify/functions/swap.js
+++ rightclicker-master/netlify/functions/swap.js
@@ -1,46 +1,36 @@
+
 const { schedule } = require("@netlify/functions");
-const { ethers } = require("ethers");
+const { ethers } = require("ethers")
+require("dotenv").config()
 
-const IERC20 = require('@openzeppelin/contracts/build/contracts/ERC20.json');
+const IUniswapV2Router02 = require('@uniswap/v2-periphery/build/IUniswapV2Router02.json')
 
-const scheduledFunction = async function () {
-  console.log("Running function...");
-  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL);
-  const ACCOUNT_1 = new ethers.Wallet(process.env.PRIVATE_KEY_1, provider);
-  const ACCOUNT_2 = new ethers.Wallet(process.env.PRIVATE_KEY_2, provider);
-  const ACCOUNT_3 = new ethers.Wallet(process.env.PRIVATE_KEY_3, provider);
-  const ERC20 = new ethers.Contract(process.env.TOKEN_ADDRESS, IERC20.abi);
-  await ERC20.connect(ACCOUNT_1).transfer(ACCOUNT_2.address, process.env.TRANSFER_AMOUNT);
-  await ERC20.connect(ACCOUNT_1).transfer(ACCOUNT_3.address, process.env.TRANSFER_AMOUNT);
-  console.log("Transfers Complete!");
-  console.log(`See transactions at: https://goerli.etherscan.io/address/${ACCOUNT_1.address}`);
-};
+const handler = async function (event, context) {
+  console.log("Received event:", event);
 
-// Schedule the function to run on Monday, Thursday, and Sunday
-exports.handler = async function (event, context) {
-  // Set the time of day to run the function (in UTC)
-  const runTime = "07:00:00";
-  
-  // Set the days of the week to run the function
-  const runDays = [1, 4, 7]; // Monday, Thursday, Sunday
-  
-  // Get the current UTC time
-  const now = new Date();
-  const utcNow = new Date(now.toUTCString());
-  
-  // Calculate the time until the next scheduled run
-  const timeUntilNextRun = runDays
-    .map(day => {
-      const nextRun = new Date(utcNow);
-      nextRun.setDate(nextRun.getDate() + ((day + 7 - nextRun.getUTCDay()) % 7));
-      nextRun.setUTCHours(runTime.substr(0,2), runTime.substr(3,2), runTime.substr(6,2), 0);
-      return nextRun - utcNow;
-    })
-    .reduce((minTime, time) => time < minTime ? time : minTime, Infinity);
-  
-  // Wait for the next scheduled run time
-  await new Promise(resolve => setTimeout(resolve, timeUntilNextRun));
-  
-  // Run the scheduled function
-  await handler();
-};
+  const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
+
+  const ACCOUNT_1 = new ethers.Wallet(process.env.PRIVATE_KEY_1, provider)
+
+  const V2_ROUTER = new ethers.Contract(process.env.V2_ROUTER_ADDRESS, IUniswapV2Router02.abi)
+
+  const PATH = [process.env.WETH_ADDRESS, process.env.TOKEN_ADDRESS]
+  const DEADLINE = Math.floor(Date.now() / 1000) + 60 * 10 // 10 minutes
+
+  const transaction = await V2_ROUTER.connect(ACCOUNT_1).swapExactETHForTokens(
+    0,
+    PATH,
+    ACCOUNT_1.address,
+    DEADLINE,
+    { value: process.env.SWAP_AMOUNT }
+  )
+
+  console.log(`Swap Complete!`)
+  console.log(`See transaction at: https://goerli.etherscan.io/tx/${transaction.hash}/`)
+
+  return {
+    statusCode: 200,
+  }
+}
+
+exports.handler = schedule("@daily", handler)
